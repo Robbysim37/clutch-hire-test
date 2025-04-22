c913e5a81fcd3d140d24ec767e043f1d5b0c180f
Robert Lewis <Robbysim37@gmail.com>
2025-04-22 14:42:42 -0400

    WIP

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navContainer">
+    <div class="logoContainer">
+      <img class="logo" :src="GreenTechLogo" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import GreenTechLogo from "@/assets/GreenTechLogo.png";
+</script>
+
+<style scoped>
+.logo {
+  height: auto;
+  width: 140.14px;
+}
+
+.logoContainer {
+  width: 310px;
+  margin-top: 30px;
+}
+
+.navContainer {
+  display: flex;
+  justify-content: center;
+}
+</style>
